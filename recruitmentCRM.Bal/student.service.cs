e211cfe70d696ec27cad63d32a7a2ebd67e2e837
mohamad awad <noza25588@gmail.com>
2022-04-19 16:04:52 +0300

    Add project files.

---
+++ recruitmentCRM.Bal/student.service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using recruitmentCRM.Dal;
+using recruitmentCRM.Dto;
+
+
+
+namespace recruitmentCRM.Bal
+{
+    public class StudentService
+    {
+        private readonly RecruitmentCRMContext Context;
+        public StudentService(RecruitmentCRMContext context)
+        {
+            Context = context;
+        }
+        public IEnumerable<Students> GetAll()
+        {
+            return Context.Set<Students>().ToList();
+        }
+
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Add(StudentDto studentDto)
+        {
+            Mappers mappers = new Mappers();
+            var Student = mappers.Map<StudentDto, Students>(studentDto);
+
+            Context.Set<Students>().Add(Student);
+        }
+    }
+}
